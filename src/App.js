e8b652a6e019a6b869f62f673c9fbfd4d8f4edfa
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-10-22 19:53:06 +0300

    design the footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
 import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import About from "./pages/About";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <About />
         </Route>
         <Route path="/blog">
@@ -23,6 +27,7 @@ function App() {
           <Services />
         </Route>
       </Switch>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import { IconButton } from "@material-ui/core";
+
+import { Link } from "react-router-dom";
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__top">
+        <div className="footer__links">
+          <Link to="/about">About Us</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <div className="footer__right">
+          <span>Find Us</span>
+
+          <div className="footer__social-links">
+            <IconButton>
+              <WhatsAppIcon />
+            </IconButton>
+            <IconButton>
+              <InstagramIcon />
+            </IconButton>
+            <IconButton>
+              <FacebookIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+      <span>
+        {" "}
+        Copyright - {new Date().getUTCFullYear()} www.aresdetraduccion.com ALL
+        RIGHTS RESERVED{" "}
+      </span>
+    </div>
+  );
+};
+
+export default Footer;

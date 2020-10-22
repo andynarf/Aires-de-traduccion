1af5898a339da810e124575a09168f38ac6e06ad
Cyrus Kiprop <cyruskiprop254@gmail.com>
2020-10-22 22:06:24 +0300

    design the about section

---
+++ src/components/Languages/Language.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Language.css";
+
+const Language = () => {
+  return (
+    <div className="language">
+      <button className="language-active">Eng</button>
+      <button>Esp</button>
+      <button>Fra</button>
+    </div>
+  );
+};
+
+export default Language;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,21 +1,37 @@
-import React from 'react'
-import NavBar from './../components/navbar/NavBar'
+import React from "react";
+import NavBar from "./../components/navbar/NavBar";
+
+import feature_img from "../assets/images/nosotros.jpeg";
+import "./About.css";
+import Language from "../components/Languages/Language";
+import { Link } from "react-router-dom";
 
 const About = () => {
-    return (
-        <div>
-            <NavBar />
-            {/* 
-            <navBar /> 
+  return (
+    <div className="about">
+      <div className="about__feature-img">
+        <Language />
+        <span>Welcome</span>
+      </div>
+      <div className="about__info">
+        <p>
+          Our company specializes in translation as well as other linguistic
+          services <br />
+          both for individuals and private companies.
+          <br />
+          Our team is composed by
+          <br />
+          professional translators and editors specialized in several languages
+          <br />
+          and subject matters.
+        </p>
+        <p>
+          Learn more about our
+          <Link to="/services">Sevices.</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-            <body>
-
-            <footer>
-            
-            */}
-            
-        </div>
-    )
-}
-
-export default About
+export default About;
